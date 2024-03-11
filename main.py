9f1970ef14bca8cdecf9ef99b4f2d29c8da5f452
Gladkov Sergey <melcor2006@mail.ru>
2024-03-11 20:37:16 +0300

    second commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from src.api import HeadHunterAPI
 from src.JSONSaver import JSONSaver
+from src.vacancy import Vacancy
 
 
 def user_interaction():
+	""" Функция для взаимодействия с пользователем. """
 	while True:
 		user_input = input(f'Хотите посмотреть отложенные вакансии нажмите 1\n'
 		f'Хотите удалить отложенные вакансии нажмите 2\n'
@@ -14,13 +16,20 @@ def user_interaction():
 			JSONSaver.delete_vacancy()
 			print('Вакансии удалены.')
 		elif user_input == '3':
+			current_vacancies = []
 			user_input = input('Какую вакансию вы ищете?')
 			hh_api = HeadHunterAPI()
 			hh_vacancies = hh_api.get_vacancies(user_input)
-			# user_city = input('В каком городе?')
-
+			user_city = input('В каком городе?')
+			# filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
+			# salary_range = input("Введите диапазон зарплат: ")
 			for i in hh_vacancies:
-				print(i)
+				if i['area']['name'] == user_city:
+					x = Vacancy(i['name'], i['url'], i['salary'], i['snippet'])
+					# current_vacancies.append(x)
+					x = Vacancy.to_json(x)
+					JSONSaver.add_vacancy(x)
+			# print(current_vacancies)
 		else:
 			print('Программа завершена.')
 			break
--- src/api.py
+++ src/api.py
@@ -11,6 +11,7 @@ class AbsHeadHunterAPI(ABC):
 class HeadHunterAPI(AbsHeadHunterAPI):
 
 	def get_vacancies(self, user_input):
+		""" Получение вакансий с hh.ru в формате JSON. """
 		url = 'https://api.hh.ru/vacancies'
 		params = {'text': user_input, 'search_field': 'name', 'per_page': 100}
 		response = requests.get(url, params=params)
